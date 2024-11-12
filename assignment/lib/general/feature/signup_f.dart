a328547443019f0eb63b8a86e37fe8f079c18136
TCF1209 <138558927+TCF1209@users.noreply.github.com>
2024-11-12 13:58:48 +0800

    submit by cf

--- assignment/lib/general/feature/signup_f.dart
+++ assignment/lib/general/feature/signup_f.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 
 const String databaseURL = 'https://fitness-app-490b6-default-rtdb.asia-southeast1.firebasedatabase.app/';
 
-Future<void> saveNewUser(String? userId, String username, String gender, String height, String password, String secretpas) async{
+Future<bool> saveNewUser(String? userId, String username, String gender, String height, String password, String secretpas) async {
   final url = Uri.parse('$databaseURL/user/user.json');
 
   final userData = {
@@ -15,9 +15,10 @@ Future<void> saveNewUser(String? userId, String username, String gender, String 
   };
 
   try {
-    await http.post(url, body: json.encode(userData));
-  }
-  catch (error){
-    throw Exception('$error');
+    final response = await http.post(url, body: json.encode(userData));
+    return response.statusCode == 200;
+  } catch (error) {
+    print(error); // Optionally handle error better or log somewhere
+    return false;
   }
 }
--- assignment/lib/general/view/singup.dart
+++ assignment/lib/general/view/singup.dart
@@ -1,32 +1,19 @@
 import 'package:flutter/material.dart';
 import '../feature/signup_f.dart';
 
-class SignUp extends StatefulWidget{
+class SignUp extends StatefulWidget {
   const SignUp({super.key});
 
   @override
   _SignUpState createState() => _SignUpState();
 }
 
-class _SignUpState extends State<SignUp>{
-//gender, height, password, secretpas, username
-//username, gender, height, password, secretpas
-
-  late TextEditingController _usernameController;
-  late bool _isMale;
-  late TextEditingController _userheightController;
-  late TextEditingController _userpasswordController;
-  late TextEditingController _usersecretpasController;
-
-  @override
-  void initState() {
-    super.initState();
-    _usernameController = TextEditingController(text: "");
-    _userheightController = TextEditingController(text: "");
-    _userpasswordController = TextEditingController(text: "");
-    _usersecretpasController = TextEditingController(text: "");
-    _isMale = true;
-  }
+class _SignUpState extends State<SignUp> {
+  TextEditingController _usernameController = TextEditingController();
+  bool _isMale = true; // Default to male, update if needed.
+  TextEditingController _userheightController = TextEditingController();
+  TextEditingController _userpasswordController = TextEditingController();
+  TextEditingController _usersecretpasController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -35,60 +22,102 @@ class _SignUpState extends State<SignUp>{
         title: const Text('Sign Up'),
       ),
       body: Center(
-        child: Padding(
+        child: SingleChildScrollView(
           padding: const EdgeInsets.all(24.0),
           child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(controller: _usernameController, decoration: const InputDecoration(labelText: 'Username', 
-            border: OutlineInputBorder(),
-            ),
-            ),
-            const SizedBox(height: 20,width: 20),
-          Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            IconButton(
-              icon: Icon(Icons.male, color: _isMale? Colors.blue : Colors.grey),
-              onPressed: () => setState(() => _isMale = true)
-            ),
-            IconButton(
-              icon: Icon(Icons.female, color: !_isMale? Colors.pink : Colors.grey),
-              onPressed: () => setState(() => _isMale = false)
-            ),
-          ],
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: _usernameController,
+                decoration: const InputDecoration(
+                  labelText: 'Username',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.male, color: _isMale ? Colors.blue : Colors.grey),
+                    onPressed: () => setState(() => _isMale = true)
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.female, color: !_isMale ? Colors.pink : Colors.grey),
+                    onPressed: () => setState(() => _isMale = false)
+                  ),
+                ],
+              ),
+              TextField(
+                controller: _userheightController,
+                decoration: const InputDecoration(
+                  labelText: 'Height',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                controller: _userpasswordController,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                controller: _usersecretpasController,
+                decoration: const InputDecoration(
+                  labelText: 'Secret Word',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  bool isSuccess = await saveNewUser(
+                    null, _usernameController.text, 
+                    _isMale ? 'male' : 'female', _userheightController.text,
+                    _userpasswordController.text, _usersecretpasController.text
+                  );
+                  if (isSuccess) {
+                    _showSuccessDialog();
+                  }
+                },
+                child: const Text('Sign Up'),
+              ),
+            ],
           ),
-          TextField(controller: _userheightController, decoration: const InputDecoration(labelText: 'Height',
-            border: OutlineInputBorder(),
-            ),
-            ),
-            const SizedBox(height: 20,width: 20),
-          TextField(controller: _userpasswordController, decoration: const InputDecoration(labelText: 'Password',
-          border: OutlineInputBorder(),
-          ),
-          ),
-          const SizedBox(height: 20,width: 20),
-          TextField(controller: _usersecretpasController, decoration: const InputDecoration(labelText: 'Secret Word',
-          border: OutlineInputBorder(),
-          ),
-          ),
-          const SizedBox(height: 20,width: 20),
-          ElevatedButton(
-            onPressed: () async {
-              await saveNewUser(
-                null, _usernameController.text, 
-                _isMale ? 'male' : 'female', _userheightController.text,
-                _userpasswordController.text, _usersecretpasController.text
-              );
-              Navigator.pop(context);
-            },
-            child: const Text('Sign Up',),
-          ),
-        ],
         ),
-      ),
       ),
     );
   }
-  
+
+  Future<void> _showSuccessDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // User must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Congratulations!'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: const <Widget>[
+                Text('You have successfully signed up.'),
+                Text('Welcome to FITSPHERE!'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+                Navigator.of(context).pop(); // Navigate back to the previous screen
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
