8fd958711a30b0a139000f2cc1cd271725c0c1c6
Karen031027 <karenguang2003@gmail.com>
2024-11-13 15:57:42 +0800

    MainActivity

---
+++ assignment/android/app/src/main/kotlin/com/fitness/assignment/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.fitness.assignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+    // This class doesn't need any additional code
+}
